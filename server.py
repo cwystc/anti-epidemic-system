dfcaeede6bd19fcc7d7a249f627efce6a3ebf414
panole <41289420+panole@users.noreply.github.com>
2023-05-27 02:20:37 +0800

    Update server.py
    q5-q9

--- server.py
+++ server.py
@@ -51,6 +51,28 @@ def test_site_name2test_site_number(test_site_name):
         res = x
     return res
 
+def test_site_number2test_site_name(test_site_number):
+    query = ('''
+        SELECT test_site_name FROM `testing sites`
+        WHERE test_site_number = %s
+        ''')
+    cursor.execute(query, (test_site_number,))
+    res = None
+    for x, in cursor:
+        res = x
+    return res
+
+def ID_number2person_name(ID_number):
+    query = ('''
+        SELECT person_name FROM person
+        WHERE ID_number = %s
+        ''')
+    cursor.execute(query, (ID_number,))
+    res = None
+    for x, in cursor:
+        res = x
+    return res
+
 @app.route('/') #index page
 def index():
     return ('''
@@ -72,6 +94,16 @@ def index():
     <a href="/query2"> query the locations a person has been to between date A and date B </a>
     <br>
     <a href="/query3"> query all the persons that have been to a particular location and the dates </a>
+    <br>
+    <a href="/query5"> query the most recent positive record of the person </a>
+    <br>
+    <a href="/query6"> query all the positive records in the test site </a>
+    <br>
+    <a href="/query7"> query all the positive records after some time </a>
+    <br>
+    <a href="/query8"> query all people who have traveled to dangerous places after some day but have not been tested after some time </a>
+    <br>
+    <a href="/query9"> query the fastest ten people belonged to the advisor who have been tested in some day </a>
     ''')
 
 @app.route('/update1') #add/update a location (and its risk level)
@@ -254,3 +286,126 @@ def query3():
     if not ret:
         return 'None'
     return ret
+
+@app.route('/query5') #query the most recent positive record of the person
+def query5():
+    person_name = request.args.get('person_name', '')
+    if not person_name:
+        return render_template('form.html', t1 = 'person_name')
+    person_id=person_name2ID_number(person_name)
+    query = ('''
+        SELECT test_result,test_datetime,test_site FROM `test record`
+        WHERE `tested_person` = %s and test_datetime>=all(
+        SELECT test_datetime FROM `test record`
+        WHERE `tested_person` = %s
+        )
+        ''')
+    cursor.execute(query, (person_id,person_id))
+    for test_result,test_datetime,test_site in cursor:
+        if test_result:
+            return '{} tested positive most recently in test site {} on {}'.format(person_name, test_site_number2test_site_name(test_site),test_datetime)
+        else:
+            return '{} tested negative most recently in test site {} on {}'.format(person_name, test_site_number2test_site_name(test_site),test_datetime)
+    return 'The queried test record cannot be found!!!'
+
+
+@app.route('/query6') #query all the positive records in the test site
+def query6():
+    test_site_name = request.args.get('test_site_name', '')
+    if not test_site_name:
+        return render_template('form.html', t1 = 'test_site_name')
+    test_site_number=test_site_name2test_site_number(test_site_name)
+    query = ('''
+        SELECT test_datetime,tested_person FROM `test record`
+        WHERE `test_site` = %s and test_result = 1
+        ''')
+    cursor.execute(query, (test_site_number,))
+    res=''
+    for test_datetime,tested_person in cursor.fetchall():
+        res+='{} tested positive in test site {} on {} <br>'.format(ID_number2person_name(tested_person), test_site_name, test_datetime)
+    if not res:
+        return 'The queried test record cannot be found!!!'
+    return res
+
+@app.route('/query7') #query all the positive records after some time
+def query7():
+    test_datetime = request.args.get('test_datetime', '')
+    if not test_datetime:
+        return render_template('form.html', t1 = 'test_datetime')
+    query = ('''
+        SELECT tested_person,test_site,test_datetime FROM `test record`
+        WHERE `test_datetime`>= %s and test_result = 1
+        ''')
+    cursor.execute(query, (test_datetime,))
+    res=''
+    for tested_person,test_site,t_datetime in cursor.fetchall():
+        res+='{} tested positive in test site {} on {} <br>'.format(ID_number2person_name(tested_person), test_site_number2test_site_name(test_site), t_datetime)
+    if not res:
+        return 'The queried test record cannot be found!!!'
+    return res
+
+@app.route('/query8') #query all people who have traveled to dangerous places after some day but have not been tested after some time
+def query8():
+    risk_level = request.args.get('risk_level', '')
+    travel_date=request.args.get('travel_date','')
+    test_datetime = request.args.get('test_datetime','')
+    if not test_datetime or not risk_level or not travel_date:
+        return render_template('form.html', t1='risk_level', t2='travel_date', t3 = 'test_datetime')
+    if risk_level=='low':
+        query = ('''
+            SELECT traveler FROM `travel record`
+            WHERE `travel_date` >= %s
+            EXCEPT
+            SELECT tested_person FROM `test record`
+            WHERE  `test_datetime` >= %s
+            ''')
+    elif risk_level=='medium':
+        query = ('''
+            SELECT traveler FROM `travel record`
+            WHERE `travel_date` >= %s and `travel_location` not in (SELECT `location_number` FROM `location` WHERE risk_level='low')
+            EXCEPT
+            SELECT tested_person FROM `test record`
+            WHERE  `test_datetime` >= %s
+            ''')
+    elif risk_level=='high':
+        query = ('''
+            SELECT traveler FROM `travel record`
+            WHERE `travel_date` >= %s and `travel_location` in (SELECT `location_number` FROM `location` WHERE risk_level='high')
+            EXCEPT
+            SELECT tested_person FROM `test record`
+            WHERE  `test_datetime` >= %s
+            ''')
+    else:
+        return 'the risk level of a location must be low, medium or high'
+    cursor.execute(query, (travel_date,test_datetime))
+    res=''
+    for tested_person, in cursor.fetchall():
+        res+='{}, '.format(ID_number2person_name(tested_person))
+    if not res:
+        return 'The queried person cannot be found!!!'
+    return res[:-2]+' have traveled to dangerous places after {} but have not been tested after {}'.format(travel_date,test_datetime)
+
+
+@app.route('/query9') #query the fastest ten people belonged to the advisor who have been tested in some day
+def query9():
+    test_date = request.args.get('test_date', '')
+    advisor_name=request.args.get('advisor_name','')
+    if not test_date or not advisor_name:
+        return render_template('form.html', t1 = 'test_date',t2='advisor_name')
+    advisor_id=person_name2ID_number(advisor_name)
+    query = ('''
+        SELECT tested_person,test_site,test_datetime FROM `test record`
+        WHERE DATE(`test_datetime`)=%s and tested_person in (SELECT ID_number FROM `person` WHERE advisor=%s) 
+        ORDER BY test_datetime asc
+        ''')
+    cursor.execute(query, (test_date,advisor_id))
+    res=''
+    count=0
+    for tested_person,test_site,t_datetime in cursor.fetchall():
+        count+=1
+        if count==11:
+            break
+        res+='{} has been tested in test site {} on {} <br>'.format(ID_number2person_name(tested_person), test_site_number2test_site_name(test_site), t_datetime)
+    if not res:
+        return 'The queried person cannot be found!!!'
+    return res
